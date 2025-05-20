adb0144be5b44e36eb86fe9888f2eba4021655b9
nemias gomes <nemiasgomes83@gmail.com>
2025-05-20 15:56:27 -0300

    UM PASSO DE CADE VEZ

---
+++ treinamento01/ex004.js
@@ -0,0 +1,7 @@
+var velocidade = 80
+
+console.log(` A velocidade do seu carro é ${velocidade}`);
+if (velocidade > 60) {
+    console.log('Velocidade excedida');
+}
+console.log("Dirija sempre usando o cinto de segurança")
---
+++ treinamento01/ex005.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país != 'EUA') {
+    console.log('Você é Estrangeiro')}
+    else {
+        console.log('Você é  Americano')
+    }
